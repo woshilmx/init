b3f74d6bed00bb1a7e0fffeae4457770837f29fe
woshilmx <2943713364@qq.com>
2023-10-30 16:47:46 +0800

    校园图书管理功能，图书信息包括：图书id，图书名称，图书规格，图书价格，图书数量

--- src/main/java/com/yupi/project/controller/BookController.java
+++ src/main/java/com/yupi/project/controller/BookController.java
@@ -1,1 +1,26 @@
-在`com.yupi.project.controller.BookController`文件中定义`BookController`类
+package com.yupi.project.controller;
+
+import com.yupi.project.model.entity.Book;
+import com.yupi.project.service.BookService;
+import com.yupi.project.vo.BaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/books")
+    public BaseResponse<Book> addBook(@RequestBody Book book) {
+        Book addedBook = bookService.addBook(book);
+        return new BaseResponse<>(addedBook);
+    }
+}
