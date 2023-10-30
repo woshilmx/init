8b72d52a0f4dd5163f2a4583155b9f46f4b067ce
woshilmx <2943713364@qq.com>
2023-10-30 14:59:17 +0800

    资产登记功能：支持添加新的固定资产信息，包括资产编号、名称、规格、采购日期、采购价格。

--- src/main/java/com/example/model/AssetResponse.java
+++ src/main/java/com/example/model/AssetResponse.java
@@ -9,5 +9,42 @@ public class AssetResponse {
     private String type;
     private LocalDateTime createTime;
 
-    // Constructors, getters, and setters for the response parameters
+    public AssetResponse(int id, String name, String type, LocalDateTime createTime) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.createTime = createTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
 }
