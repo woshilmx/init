8542a02e0dde901e4c0f91550d12f798ad65474c
woshilmx <2943713364@qq.com>
2023-10-30 15:09:05 +0800

    资产登记功能：支持添加新的固定资产信息，包括资产编号、名称、规格、采购日期、采购价格。

--- src/main/java/com/example/controller/AssetController.java
+++ src/main/java/com/example/controller/AssetController.java
@@ -6,7 +6,7 @@ import com.example.service.AssetService;
 import com.example.validation.ValidationResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ public class AssetController {
     @Autowired
     private AssetService assetService;
 
-    @PostMapping("/assets")
+    @PutMapping("/assets")
     public ResponseEntity<AssetResponse> addAsset(@RequestBody AssetRequest assetRequest) {
         ValidationResult validationResult = validateAsset(assetRequest);
         if (!validationResult.isValid()) {
@@ -34,8 +34,6 @@ public class AssetController {
     }
 
     private ValidationResult validateAsset(AssetRequest assetRequest) {
-        // Perform validation logic and return the validation result
-        // Example validation logic:
         ValidationResult validationResult = new ValidationResult();
         if (assetRequest.getName() == null || assetRequest.getName().isEmpty()) {
             validationResult.addError("Name is required");
